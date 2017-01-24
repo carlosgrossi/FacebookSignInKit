d6e9ba457d6b55871e95406a0c0f0059b9e46082
Carlos Grossi <carlos.grossi@gmail.com>
2017-01-24 18:01:25 -0200

    Initial Commit

---
+++ FacebookSignInKit/FacebookSignInKit.h
@@ -0,0 +1,19 @@
+//
+//  FacebookSignInKit.h
+//  FacebookSignInKit
+//
+//  Created by Carlos Grossi on 24/01/17.
+//  Copyright © 2017 Carlos Grossi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FacebookSignInKit.
+FOUNDATION_EXPORT double FacebookSignInKitVersionNumber;
+
+//! Project version string for FacebookSignInKit.
+FOUNDATION_EXPORT const unsigned char FacebookSignInKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FacebookSignInKit/PublicHeader.h>
+
+
